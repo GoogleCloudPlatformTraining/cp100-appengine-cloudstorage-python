cf8777b969e669b291e9f2bfa15ba2051fdae495
Sharif Salah <sharif.salah@gmail.com>
2015-03-26 18:10:58 +0000

    Initial version based on Datastore exercise

---
+++ guestbook.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import jinja2
+import os
+import webapp2
+
+from google.appengine.ext import ndb
+
+
+def guestbook_key(guestbook_name='default_guestbook'):
+    return ndb.Key('Guestbook', guestbook_name)
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class Greeting(ndb.Model):
+    content = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        greetings_query = Greeting.query(
+            ancestor=guestbook_key()).order(-Greeting.date)
+        greetings = greetings_query.fetch(10)
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(entries=greetings))
+
+    def post(self):
+        greeting = Greeting(parent=guestbook_key())
+        greeting.content = self.request.get('entry')
+        greeting.put()
+        self.redirect('/')
+
+
+class Clear(webapp2.RequestHandler):
+    def post(self):
+        greetings_query = Greeting.query(
+            ancestor=guestbook_key()).fetch(keys_only=True)
+        ndb.delete_multi(greetings_query)
+        self.redirect('/')
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/clear', Clear),
+], debug=True)
